121ddc1f7036bf2d2e490b2ff89fcce2c6e7339b
Mishach <mishachkld@gmail.com>
2024-03-19 21:28:07 +0200

    git init

---
+++ DB/SetDefaultSettingsDB/drop_tables_stript.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS actor, director, movie, movie_actor;
+DROP TABLE IF EXISTS language, movie_title;
---
+++ DB/script.sql
@@ -0,0 +1,39 @@
+CREATE TABLE IF NOT EXISTS language
+(
+    ID   varchar(2)   not null, # 'ru'
+    NAME varchar(200) not null, # 'Russian'
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE IF NOT EXISTS movie_title
+(
+    TITLE       varchar(200) not null,              # 'Name film'
+    LANGUAGE_ID varchar(2)   not null, # 'ru'
+    MOVIE_ID    int          not null,
+
+    PRIMARY KEY (TITLE),
+    FOREIGN KEY FK_MT_MOVIE (MOVIE_ID)
+        REFERENCES movie (ID)
+        ON UPDATE RESTRICT
+        ON DELETE RESTRICT,
+    FOREIGN KEY FK_MT_LANGUAGE (LANGUAGE_ID)
+        REFERENCES language (ID)
+        ON UPDATE RESTRICT
+        ON DELETE RESTRICT
+);
+
+INSERT INTO language (ID, NAME)
+VALUES ('ru', 'Russian'),
+       ('en', 'English'),
+       ('de', 'German');
+
+INSERT INTO movie_title (MOVIE_ID, LANGUAGE_ID, TITLE)
+SELECT ID, 'ru', TITLE FROM movie;
+
+# SELECT TITLE, 'ru', ID  INTO movie_title FROM movie;
+
+# ALTER TABLE movie_title
+#     MODIFY LANGUAGE_ID varchar(2) not null;
+
+ALTER TABLE movie
+    DROP TITLE;
